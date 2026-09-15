0a3ff51be61d23ee2e32f2f75cae84fd349e298d
kimdohui05 <kimdohui05@gmail.com>
2026-09-15 09:14:58 +0900

    asd

---
+++ src/main/java/com/bank/yaksok/YakSokApplication.java
@@ -0,0 +1,13 @@
+package com.bank.yaksok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YakSokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YakSokApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bank/yaksok/YakSokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.yaksok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YakSokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
